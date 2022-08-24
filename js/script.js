ff305d86a75b1723f08d50651c7f5df4b02fce10
ほたてわい <82103761+hotatekainomodel@users.noreply.github.com>
2022-08-25 03:57:25 +0900

    Update script.js
    Signed-off-by: ほたてわい <82103761+hotatekainomodel@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -16,3 +16,17 @@ const typing = (element, sentence) => {
   });
 }
 typing('#typing', 'Welcome to my Portfolio');
+
+// スクロールアニメーション
+$(function () {
+  $(window).scroll(function () {
+    $('.effect-fade').each(function () {
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight) {
+        $(this).addClass('effect-scroll');
+      }
+    });
+  });
+});
